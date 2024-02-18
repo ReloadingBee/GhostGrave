825ee20977cfa84efe857548a60f709f45df505d
ReloadingBee <reloadingbee@gmail.com>
2024-02-18 16:27:39 +0200

    Updates

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,6 +4,9 @@ public class Rocket : MonoBehaviour
 {
     public float speed = 20f;
     public int damage = 10;
+    public GameObject explosionPrefab;
+    public GameObject hitPrefab;
+    public int bounceCount;
 
     private void Start()
     {
@@ -17,11 +20,25 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        Destroy(gameObject);
         var health = other.gameObject.GetComponent<Health>();
         if(health != null)
         {
             health.Damage(damage);
+        }
+
+        // Wall hit effect
+        var obj = Instantiate(hitPrefab, transform.position, Quaternion.identity);
+        obj.transform.forward = other.contacts[0].normal;
+
+        if (bounceCount > 0)
+        {
+            transform.forward = other.contacts[0].normal;
+            bounceCount--;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         }
     }
 }
